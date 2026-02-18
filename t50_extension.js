6c5ef5b1430169bf420959ad63b994b5cdfd29b9
Aaron Shaji <23353as@hvhs.school.nz>
2026-02-18 15:14:13 +1300

    Made progress on the merge game

--- t50_extension.js
+++ t50_extension.js
@@ -7,53 +7,62 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
+let ball_1;
+
+function func2Call(ball1Group) {
+  ball_1.remove();
+}
 function setup() {
 	console.log("setup: ");
-	cnv = new Canvas(windowWidth, windowHeight);
+	cnv = new Canvas(windowWidth/4, windowHeight);
 
 	world.gravity.y = 10;
 
 	createWalls();
 
-	ball_1 = new Sprite(width / 2, height / 2, 50, 'd');
-	ball_1.color = 'cyan';
-	ball_1.vel.x = 2;
-	ball_1.bounciness = 100;
-	ball_1.friction = 0;
-	ball_1.drag = 0;
+	createNewBall(width / 2, height / 2);
 }
 
 function createWalls() {
 	wallLH = new Sprite(0, height / 2, 8, height, 'k');
 	wallLH.color = 'red';
 
-
 	wallRH = new Sprite(width, height / 2, 8, height, 'k');
 	wallRH.color = 'red';
-
 
 	wallTop = new Sprite(width / 2, 0, width, 8, 'k');
 	wallTop.color = 'red';
 
-
 	wallBot = new Sprite(width / 2, height, width, 8, 'k');
 	wallBot.color = 'red';
 }
+
+// Function to create a new ball
+function createNewBall(x, y) {
+  ball1Group = new Group();
+	ball_1 = new Sprite(x, y, 50, 'd');
+	ball_1.color = 'cyan';
+	ball_1.bounciness = 0;
+	ball_1.friction = 5;
+	ball_1.drag = 1;
+}
+
+ball1Group.collides(ball1Group, func2Call);
+
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-		background('gray');
+	background('gray');
 
-			ball_1.moveTowards(mouseX, mouseY, 1);
+	ball_1.moveTowards(mouseX, mouseY, 1);
 
-			if (mouse.presses()) {
-
-			ball_1.moveTo(xPos, yPos, n);
-
+  if (mouse.presses()) {
+		createNewBall(mouseX, mouseY);
+	}
 }
-}
+
 
 /*******************************************************/
 //  END OF APP
